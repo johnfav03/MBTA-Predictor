f298761edab83bdabf4a3bc51a48012ef30bb0da
John Favret <john.favret@energysage.com>
2023-07-17 10:21:15 -0400

    debugging actions

--- eval-model.py
+++ eval-model.py
@@ -8,6 +8,7 @@ import numpy as np
 file = 'lstm-model.h5'
 model = load_model(file)
 df = pd.read_csv('data/LR-yesterday.csv')
+print(len(df))
 
 X = df[['stop_id', 'route_id', 'week_day', 'month_day', 'start_time']].values
 y = df['travel_time'].values
